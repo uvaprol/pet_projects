962d8b14058b1f97557e206460cd2446ffa868ac
uvaprol <120366489+uvaprol@users.noreply.github.com>
2023-06-21 17:46:53 +0300

    Update town_menager_console.py

--- graphs_tool/town_menager_console.py
+++ graphs_tool/town_menager_console.py
@@ -83,38 +83,50 @@ def show_town(towns):
     return
 
 def router(start, finish):
-    towns_mas=[]
+    towns_mas=[[]]
     for key in towns:
-        towns_mas.append(key)
-    towns_mas = itertools.permutations(towns_mas)
+        towns_mas[0] += key
+    n = len(towns_mas[0])
+    for i in range(2,n):
+        towns_mas += itertools.permutations(towns_mas[0],i)
     return towns_mas
 
-def way_calculade(way):
-    global towns
+def way_calculade(way, towns):
     rout = 0
+    gate = True
     for key in range(len(way)-1):
         try:
             rout += (towns[way[key]][way[key+1]])
         except:
             rout = 0
-    return rout,way
+            gate = False
+            break
+    return rout, way, gate
     
 def math_route(towns):
     start = str(input('start town: '))
     finish = str(input('finish town: '))
-    route = -1
     towns_mas = router(start, finish)
+    route = -1
+    find_rout = ''
     for i in towns_mas:
         if i[0] == start and i[-1] == finish:
-            find_way, way = way_calculade(i)
-            if find_way < route and (route != -1) and find_way > 0:
-                route = find_way
-            elif (route == -1):
-                route = find_way
+            find_way, way, gate = way_calculade(i, towns)
+            if gate:
+                if route == -1:
+                    route = find_way
+                    find_rout = way
+                if route > find_way:
+                    print(route)
+                    route = find_way
+                    find_rout = way
+         
+    way = find_rout
     str_way = f'{way[0]} -> '
     for i in range(1,len(way)-1):
         str_way += f'{way[i]} -> ' 
     print(f'{str_way}{way[-1]} == {route}')
+
 
 
 
