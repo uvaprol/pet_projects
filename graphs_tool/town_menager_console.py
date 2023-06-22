e9fc6d7e1e991ca5ede3e0308f9fd5d06153808a
uvaprol <120366489+uvaprol@users.noreply.github.com>
2023-06-22 11:45:54 +0300

    Update town_menager_console.py

--- graphs_tool/town_menager_console.py
+++ graphs_tool/town_menager_console.py
@@ -1,9 +1,14 @@
+import itertools
+
+
 towns = {
-    'a':{'b':5, 'c':6, 'd':7},
-    'b':{'a':5},
-    'c':{'a':6},
-    'd':{'a':7},
+    'a': {'b': 2, 'd': 1},
+    'b': {'a': 2, 'c': 3, 'd': 3},
+    'c': {'b': 3, 'd': 3, 'e': 2},
+    'd': {'a': 1, 'b': 3, 'c': 3},
+    'e': {'b': 2}
 }
+
 def create_new_name():
     new_name = str(input('new name: '))
     while towns.get(new_name) != None:
@@ -77,23 +82,71 @@ def create_ways(towns):
         else:
             way = int(input(f'way {child_town}: '))
             parent_ways[child_town] = way
-            child_ways[parent_town] = way
-            
+            child_ways[parent_town] = way            
 def show_town(towns):
     for key, value in towns.items():
         print(key, value)
     return
 
+def router(start, finish):
+    towns_mas=[[]]
+    for key in towns:
+        towns_mas[0] += key
+    n = len(towns_mas[0])
+    for i in range(2,n+1):
+        towns_mas += itertools.permutations(towns_mas[0],i)
+    return towns_mas
+
+def way_calculade(way, towns):
+    rout = 0
+    gate = True
+    for key in range(len(way)-1):
+        try:
+            rout += (towns[way[key]][way[key+1]])
+        except:
+            rout = 0
+            gate = False
+            break
+    return rout, way, gate
+    
+def math_route(towns):
+    start = str(input('start town: '))
+    finish = str(input('finish town: '))
+    towns_mas = router(start, finish)
+    route = -1
+    find_rout = ''
+    for i in towns_mas:
+        if i[0] == start and i[-1] == finish:
+            find_way, way, gate = way_calculade(i, towns)
+            if gate:
+                if route == -1:
+                    route = find_way
+                    find_rout = way
+                if route > find_way:
+                    route = find_way
+                    find_rout = way
+         
+    way = find_rout
+    str_way = f'{way[0]} -> '
+    for i in range(1,len(way)-1):
+        str_way += f'{way[i]} -> ' 
+    print(f'{str_way}{way[-1]} == {route}')
+
+
+
+
+
 comands = {
     'create':create_town,
     'way':create_ways,
     'rename': rename_town,
-    'show':show_town
+    'show':show_town,
+    'route':math_route
 }
 
 while True:
     try:
-        comand = comands.get(str(input('select comand:\nway\nrename\nshow\ncreate\n\n:')))
+        comand = comands.get(str(input('select comand:\nway\nrename\nshow\ncreate\nroute\n\n:')))
         comand(towns)
         print('___\n___')
     except:
