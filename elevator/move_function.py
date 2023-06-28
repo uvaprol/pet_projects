aaa09615306c30b6ea93f2fa0d62f6cb872d7eb6
uvaprol <120366489+uvaprol@users.noreply.github.com>
2023-06-28 18:45:10 +0300

    Update move_function.py

--- elevator/move_function.py
+++ elevator/move_function.py
@@ -1,30 +1,7 @@
 from random import randint as rd
 from time import sleep
-def move(el, move_map):
-    
-    # # hand control
-    # while True:
-    #     pos = el['floor']
-    #     try:
-    #         move_map.append(int(input(':')))
-    #     except:
-    #         pass
-        
-        
-    # auto control
-    for _ in range(15):  
-        pos = el['floor']
-        sleep(1)
-        pk = True
-        if rd(1,10) > 7:
-                move_map.append(rd(1,10))
-                print(f':{move_map[-1]}')
-                pk = False
-        if pk:
-            print(':')
-        
-        
-        
+
+def move(el, move_map, pos):
     # code start
         # repeat check
         for i in range(len(move_map)-1):
@@ -51,24 +28,48 @@ def move(el, move_map):
                     el['floor'] = el['floor'] + 1
                     el['move'] = True
                     el['direction'] = 'up'
-                    
                 if pos > move_map[0] and (el['direction'] == 'stop' or el['direction'] == 'down'):
                     el['floor'] = el['floor'] - 1
                     el['move'] = True
                     el['direction'] = 'down'
         else:
             print(el)
+
+
+def controler(el, move_map, control):
+    
+    if control == 'auto':
+        # auto control
+        for _ in range(int(input('number of repetitions: '))):  
+            pos = el['floor']
+            sleep(1)
+            pk = True
+            if rd(1,10) > 7:
+                    move_map.append(rd(1,10))
+                    print(f':{move_map[-1]}')
+                    pk = False
+            if pk:
+                print(':')
+            move(el, move_map, pos)
             
-            
-            
-            
-            
+
+    elif control == 'hand':  
+        # hand control
+        while True:
+            pos = el['floor']
+            try:
+                move_map.append(int(input(':')))
+            except:
+                pass
+            move(el, move_map, pos)
+        
+        
 el = {
-    1: {'move': False, 'floor': 9, 'task': [], 'direction': 'stop'},
+    1: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'},
     2: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'},
     3: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'}
     
 }
-
-move(el[1], [])
+control = str(input('hand/auto: '))
+controler(el[1], [], control)
 print('stop')
