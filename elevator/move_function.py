b94084eb320e50bd097b699305a587f731ac0449
uvaprol <120366489+uvaprol@users.noreply.github.com>
2023-06-28 16:54:29 +0300

    Update move_function.py

--- elevator/move_function.py
+++ elevator/move_function.py
@@ -1,20 +1,46 @@
-def move(pos, move):
-    while pos!=move[-1]:
+def move(el, move_list):
+    # floor = move_list[-1]
+
+    # for key in el:
+    #     if el[key]['ready_move']:
+    #         number = key
+    #         distance = abs(floor - el[key]['floor'])
+    #         direction = int(floor - el[key]['floor'] >= 0) 
+    #         break
+    # try:
+    #     for key in el:
+    #         if abs(floor - el[key]['floor']) < distance and el[key]['ready_move']:
+    #             distance = abs(floor - el[key]['floor'])
+    #             number = key
+    # except:
+    #     pass
+    # print(number)
+    
+    while True:
+        pos = el['floor']
         try:
             floor = int(input(':'))
         except:
-            floor = move[-1] 
-        if pos<move[-1]:
-            if floor > pos and floor < move[-1]:
-                move.append(floor)
-            pos+=1
-        else:
-            if floor < pos and floor > move[-1]:
-                move.append(floor)
-            pos-=1
+            floor = move_list[-1] 
+        if pos<move_list[-1]:
+            if floor > pos and floor < move_list[-1]:
+                move_list.append(floor)
+            el['floor'] = el['floor'] + 1
+        elif pos>move_list[-1]:
+            if floor < pos and floor > move_list[-1]:
+                move_list.append(floor)
+            el['floor'] = el['floor'] - 1
         print(pos)
-        if len(move)>1 and move[-1]==pos:
+        if len(move_list)>1 and move_list[-1]==pos:
             print('stop')
-            del move[-1]
-move(1,[9])
+            del move_list[-1]
+            
+el = {
+    1: {'ready_move': True, 'floor': 1, 'task': [], 'derection': 'stop'},
+    2: {'ready_move': True, 'floor': 1, 'task': [], 'derection': 'stop'},
+    3: {'ready_move': True, 'floor': 1, 'task': [], 'derection': 'stop'}
+    
+}
+
+move(el[1], [9])
 print('stop')
