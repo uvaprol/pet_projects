1cabde134f1d713492604f0a4146a245a56c4768
uvaprol <120366489+uvaprol@users.noreply.github.com>
2023-06-29 11:57:19 +0300

    Update move_function.py

--- elevator/move_function.py
+++ elevator/move_function.py
@@ -1,43 +1,53 @@
 from random import randint as rd
 from time import sleep
 
-def move(el, move_map, pos):
+def show(el):
+    for key in el:
+        print(key, el[key])
+    pass
+
+def move(elevator, pos):
     # code start
-        # repeat check
-        for i in range(len(move_map)-1):
-            if move_map[i] == move_map[-1]:
-                del move_map[-1]
-                break
-        move_map.sort()
-        
-        # task check
-        if move_map != []:
-            if move_map[0] == pos:
-                del move_map[0]
-                el['move'] = False
-                el['direction'] = 'stop'
-            elif move_map[-1] == pos:
-                del move_map[-1]
-                el['move'] = False
-                el['direction'] = 'stop'
-            print(el)
-            
-            # moving
-            if move_map != []:
-                if pos < move_map[-1] and (el['direction'] == 'stop' or el['direction'] == 'up'):
-                    el['floor'] = el['floor'] + 1
-                    el['move'] = True
-                    el['direction'] = 'up'
-                if pos > move_map[0] and (el['direction'] == 'stop' or el['direction'] == 'down'):
-                    el['floor'] = el['floor'] - 1
-                    el['move'] = True
-                    el['direction'] = 'down'
-        else:
-            print(el)
-
-
-def controler(el, move_map, control):
+    el = elevator[1]
+     
+    # repeat check
+    for i in range(len(el['task'])-1):
+        if el['task'][i] == el['task'][-1]:
+            del el['task'][-1]
+            break
+    el['task'].sort()
     
+    # task check
+    if el['task'] != []:
+        if el['task'][0] == pos:
+            del el['task'][0]
+            el['move'] = False
+            el['status'] = 'pause'
+        elif el['task'][-1] == pos:
+            del el['task'][-1]
+            el['move'] = False
+            el['status'] = 'pause'
+        show(elevator)
+        
+        # moving
+        if el['task'] != []:
+            if pos < el['task'][-1] and (el['status'] == 'stop' or el['status'] == 'up'):
+                el['floor'] = el['floor'] + 1
+                el['move'] = True
+                el['status'] = 'up'
+            elif pos > el['task'][0] and (el['status'] == 'stop' or el['status'] == 'down'):
+                el['floor'] = el['floor'] - 1
+                el['move'] = True
+                el['status'] = 'down'
+            elif el['status'] == 'pause':
+                el['status'] = 'stop'
+    else:
+        el['status'] = 'stop'
+        show(elevator)
+
+
+def controler(elevator, control):
+    el = elevator[1]
     if control == 'auto':
         # auto control
         for _ in range(int(input('number of repetitions: '))):  
@@ -45,12 +55,12 @@ def controler(el, move_map, control):
             sleep(1)
             pk = True
             if rd(1,10) > 7:
-                    move_map.append(rd(1,10))
-                    print(f':{move_map[-1]}')
+                    el['task'].append(rd(1,10))
+                    print(f":{el['task'][-1]}")
                     pk = False
             if pk:
                 print(':')
-            move(el, move_map, pos)
+            move(elevator, pos)
             
 
     elif control == 'hand':  
@@ -58,18 +68,18 @@ def controler(el, move_map, control):
         while True:
             pos = el['floor']
             try:
-                move_map.append(int(input(':')))
+                el['task'].append(int(input(':')))
             except:
                 pass
-            move(el, move_map, pos)
+            move(elevator, pos)
         
         
 el = {
-    1: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'},
-    2: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'},
-    3: {'move': False, 'floor': 1, 'task': [], 'direction': 'stop'}
+    1: {'move': False, 'floor': 1, 'task': [], 'status': 'stop'},
+    2: {'move': False, 'floor': 1, 'task': [], 'status': 'stop'},
+    3: {'move': False, 'floor': 1, 'task': [], 'status': 'stop'}
     
 }
 control = str(input('hand/auto: '))
-controler(el[1], [], control)
+controler(el, control)
 print('stop')
